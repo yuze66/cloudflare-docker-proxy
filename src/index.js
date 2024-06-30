5c9226b32c0da6a7e6fc15857d0718d022498ff5
yuze <35136070+yuze66@users.noreply.github.com>
2024-06-30 20:25:16 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import DOCS from './help.html'
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -7,17 +9,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.mirror.datashare.edu.eu.org": dockerHub,
+  "quay.mirror.datashare.edu.eu.org": "https://quay.io",
+  "gcr.mirror.datashare.edu.eu.org": "https://gcr.io",
+  "k8s-gcr.mirror.datashare.edu.eu.org": "https://k8s.gcr.io",
+  "k8s.mirror.datashare.edu.eu.org": "https://registry.k8s.io",
+  "ghcr.mirror.datashare.edu.eu.org": "https://ghcr.io",
+  "cloudsmith.mirror.datashare.edu.eu.org": "https://docker.cloudsmith.io",
+  "ecr.mirror.datashare.edu.eu.org": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.mirror.datashare.edu.eu.org": dockerHub,
 };
 
 function routeByHosts(host) {
@@ -42,6 +44,15 @@ async function handleRequest(request) {
         status: 404,
       }
     );
+  }
+  // return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
   }
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
